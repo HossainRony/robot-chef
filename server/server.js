1aa5fa5ec6625a6e4128d918e679ab2d3eba2699
Mohammad Hossain <mhoss247@my.centennialcollege.ca>
2024-07-09 09:44:36 -0400

    adeed dummy server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+  res.send('Robot chef working!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});

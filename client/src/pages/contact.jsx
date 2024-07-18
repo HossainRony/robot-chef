1fd884dce9fcc65e02ac7336356ca86e2e345d8e
Absar Siddiqui <absarvpn@gmail.com>
2024-07-18 18:41:58 -0400

    added about & contact page

---
+++ client/src/pages/contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact Robot Chef</h1>
+            <p>We'd love to hear from you! Whether you have questions, feedback, or need assistance, get in touch with us.</p>
+        </div>
+    )
+}
+
+export default Contact;

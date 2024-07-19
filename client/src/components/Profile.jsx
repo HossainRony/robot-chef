f83847c4bf12b4f05a4c734b59952ba21c06a32b
Lissette <lgorrinr@my.centennialcollege.ca>
2024-07-19 13:10:02 -0400

    Created Home component

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Profile = () => (
+  <div>
+    <h1>My Profile</h1>
+    <p>Manage your profile information here.</p>
+  </div>
+);
+
+export default Profile;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,9 @@
 import React, { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Recipes from './components/Recipes';
+import Profile from './components/Profile';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -7,6 +11,13 @@ const root = createRoot(container);
 root.render(
     <React.StrictMode>
         <h1> Robot Chef </h1>
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/recipes" component={Recipes} />
+                <Route path="/profile" component={Profile} />
+            </Switch>
+        </Router>
     </React.StrictMode>
 
 

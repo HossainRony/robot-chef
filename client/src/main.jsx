f49e6e5a3310fad21ee1e7731cb0e2da4559bdee
Ka Wai Yim <yimkawaii@gmail.com>
2024-07-19 20:45:30 -0400

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,6 +15,8 @@ root.render(
         <h2>Team </h2>
         <h3>Absar, Lissette, Ka Wai, Mohammad, Allen</h3>
 
+        <p>Robot Chef is a web app designed to enhance cooking by offering personalized recipe recommendations, ingredient management, meal planning, and interactive cooking guides. Users can find recipes based on their preferences and available ingredients, track their pantry, and plan meals. ​</p>
+        <p>Integration with smart kitchen devices and grocery delivery services aims to make cooking more efficient and enjoyable, promoting healthier eating habits and reducing food waste.​</p>
 
 
         <Router>
